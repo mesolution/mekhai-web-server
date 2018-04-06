208f6f6e76c6c8a8ec0e409cc4878d8ff64576ca
Me Solution <37895651+mesolution@users.noreply.github.com>
2018-04-06 14:43:00 +0700

    Update entrypoint.sh

--- Apache-PHP7.1/entrypoint.sh
+++ Apache-PHP7.1/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-
+certbot --apache
+certbot certonly -n -d mekhai.com -d www.mekhai.com
+certbot renew --dry-run 
 source /etc/apache2/envvars
 #tail -F /var/log/apache2/* &
 exec apache2 -D FOREGROUND
